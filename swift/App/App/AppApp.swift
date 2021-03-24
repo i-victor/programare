b2d7391a8ac09112b5ebebb45ee1a490d89dd618
i-victor <58788899+i-victor@users.noreply.github.com>
2021-03-24 19:31:55 +0200

    swift

---
+++ swift/App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Ilies Victor on 21/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
