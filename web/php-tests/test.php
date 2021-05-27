2f89858a0793630e1c33d7fc7d0c738e61b9d1a1
i-victor <58788899+i-victor@users.noreply.github.com>
2021-05-27 21:13:17 +0300

    php-tests

--- web/php-tests/test.php
+++ web/php-tests/test.php
@@ -1,5 +1,42 @@
 <?php
 
-phpinfo();
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_STRICT);
+date_default_timezone_set('UTC');
+
+echo '<pre>';
+
+$arr = [];
+//$map = new WeakMap;
+$obj = new stdClass;
+$arr[42] = &$obj;
+//var_dump($arr);
+//$map[$obj] = 42;
+//var_dump($map);
+// object(WeakMap)#1 (1) {
+//   [0]=>
+//   array(2) {
+//     ["key"]=>
+//     object(stdClass)#2 (0) {
+//     }
+//     ["value"]=>
+//     int(42)
+//   }
+// }
+
+// The object is destroyed here, and the key is automatically removed from the weak map.
+$obj = null;
+unset($obj);
+
+var_dump($arr);
+//var_dump($map);
+// object(WeakMap)#1 (0) {
+// }
+
+
+$a = 2;
+$b =& $a;
+$a = 0;
+echo 'B este: '.$b;
 
 // #end php code
